de9de1d31278776584175371ce3221344e6e5722
sabri-rihab <rihabsabri21@gmail.com>
2026-01-18 23:41:11 +0100

    category calsse and functions

--- tests/library_test.php
+++ tests/library_test.php
@@ -1,11 +1,15 @@
 <?php
 require_once __DIR__ . '/../src/Repositories/BookRepository.php';
+require_once __DIR__ . '/../src/Repositories/AuthorRepository.php';
+require_once __DIR__ . '/../src/Repositories/CategoryRepository.php';
 
 
-// test add book :
 $bookRepo = new BookRepository();
-$book = new Book(1001, 'before the coffee gets cold', 2008, 'available');
+$authorRepo = new AuthorRepository();
+$catRepo = new CategoryRepository();
 
+//*****************    test add book    *********************** :
+// $book = new Book(1001, 'before the coffee gets cold', 2008, 'available');
 // $result = $bookRepo->addBook($book);
 // echo $result ? "Book inserted\n" : "Insert failed\n";
 
@@ -25,5 +29,91 @@ $book = new Book(1001, 'before the coffee gets cold', 2008, 'available');
 // }
 
 //****************** test avaibality in library branchs *****************
-$availability = $bookRepo->getAvailabilityByBranch(2);
-print_r($availability);
+// $availability = $bookRepo->getAvailabilityByBranch(2);
+// print_r($availability);
+
+
+
+
+//--------------------------------------------------------------------------------------------------
+// => AUTHOR:
+//test ajout
+// $author = new Author(1,'Haruki Murakami','Japanese novelist','Japanese','1949-01-12',null,'Literature');
+// echo $authorRepo->addAuthor($author) ? "Author added\n" : "Insert failed\n";
+
+
+//find by id test:
+// $found = $authorRepo->findById(1);
+// echo $found ? $found->getName() . "\n" : "Not found\n";
+
+
+//find by author name 
+// $list = $authorRepo->searchByName('Haruki Murakami');
+// foreach ($list as $a) {
+//     echo $a->getId() . "\n";
+//     echo $a->getName() . "\n";
+//     echo $a->getNationality() . "\n";
+//     echo $a->getBirthDate() . "\n\n\n";
+
+// }
+
+
+//get books by author:
+// $books = $authorRepo->getBooksByAuthor(1);
+// print_r($books);
+
+
+
+
+//-------------------------------------------------------------------------------------------------
+// => catg
+// add
+// $cat = new Category(1, 'Literature');
+// echo $catRepo->addCategory($cat) ? "Category added\n" : "Insert failed\n";
+
+
+//get all :
+// $cats = $catRepo->getAll();
+// foreach ($cats as $c) {
+//     echo $c->getName() . "\n";
+// }
+
+
+// find by name :
+// $found = $catRepo->findByName('Literature');
+// echo $found ? $found->getName() . "\t" .  $found->getID(): "Not found\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
